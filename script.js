6385bcb12179a1c3924443ed044b8b0a67791d06
stefanKuijers <>
2023-05-22 12:08:08 +0200

    refactor: changing towards using one btn handler

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 window.addEventListener("load", function () {
     const Tasks = {
-        app: document.querySelector('[data-js-app]'),
+        // app: document.querySelector('[data-js-app]'),
 
-        init: function () {
+        init: function (rootSelector) {
+            this.app = document.querySelector(rootSelector);
+
             this.addButton = this.app.querySelector('[data-js-btn="add"]');
             this.clearButton = this.app.querySelector('[data-js-btn="clear"]');
+            this.buttons = this.app.querySelectorAll('[data-js-btn]');
             this.list = this.app.querySelector('[data-js-list]');
             this.inputField = this.app.querySelector("input");
+
+            this.addListeners();
+        },
+
+        addListeners: function () {
+            this.buttons.forEach(el => {
+                el.addEventListener("click", this.handleBtnClick);
+            })
+
+            // this.addButton.addEventListener("click", function(e) {
+            //     this.addTask();
+            // });
+            // this.clearButton.addEventListener("click", function(){
+            //     this.clearTask();
+            // });
+            
+            
+            this.list.addEventListener("click", function(event) {
+                if (event.target.tagName === "LI") {
+                    this.doneTask(event.target);
+                }
+            });
+        
+        },
+
+        handleBtnClick: function(event) {
+            let el = event.target;
+            let action = el?.attributes?.data;
+            
+            if (action == 'add') {
+                this.addTask()
+            }
+
+            if (action == 'clear') {
+                this.clearTask()
+            }
         },
 
         newElement: function (input) {
@@ -37,22 +76,13 @@ window.addEventListener("load", function () {
             };
         }
     };
-    Tasks.init();
-    
-    
-    Tasks.addButton.addEventListener("click", function() {
-        Tasks.addTask();
-    });
-    
-    
-    Tasks.list.addEventListener("click", function(event) {
-        if (event.target.tagName === "LI") {
-            Tasks.doneTask(event.target);
-        }
-    });
 
-    Tasks.clearButton.addEventListener("click", function(){
-        Tasks.clearTask();
-    });
+    /**
+     * Consumer Programmer
+     */
+    Tasks.init('[data-js-app]');
+    
+    
+    
     
 });
